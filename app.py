7fffe97650e04baf6004a9bc816aee3c9af8594e
inoc <nogasobaki23@gmail.com>
2022-10-19 17:46:58 +0300

    WIP on update user watchlist in SQL database

--- app.py
+++ app.py
@@ -29,11 +29,6 @@ class Movie(db.Model):
         return f'<Movie "{self.title[:20]}...">'
 
 
-@app.route('/')
-def homepage():
-    return render_template('index.html')
-
-
 def add_user(username):
     chel = User(username=username)
     db.session.add(chel)
@@ -43,22 +38,33 @@ def add_user(username):
     db.session.commit()
 
 
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+
 @app.route('/my_shining_app', methods=['POST', 'GET'])
 def handling():
     if request.method == 'GET':
-        return render_template('app_form.html')
-
-    unames = request.form.getlist('username')
+        return render_template('app-form.html')
+    
+    usernames = {}
+    for i in range(len(request.form)):
+        if len(request.form.getlist('update-db' + str(i))) != 0:
+            usernames[request.form.getlist('username' + str(i))[0]] = True
+        elif len(request.form.getlist('username' + str(i))) != 0:
+            usernames[request.form.getlist('username' + str(i))[0]] = False
 
     films_with_weight = {}
-
-    for username in unames:
+    for username in usernames:
+        if usernames[username]:
+            pass
 
         if not User.query.filter_by(username=username).first():
             try:
                 add_user(username)
             except ValueError:
-                return render_template('no_such_user.html')
+                return render_template('no-such-user.html')
 
         for i in User.query.filter_by(username=username).first().movies:
             if i.title not in films_with_weight:
@@ -67,11 +73,11 @@ def handling():
 
     films_intersec = []
     for i in films_with_weight:
-        if films_with_weight[i] == len(unames):
+        if films_with_weight[i] == len(usernames):
             films_intersec.append(i)
 
     random.shuffle(films_intersec)
-    f = films_intersec[:len(unames) + 1]
+    f = films_intersec[:len(usernames) + 1]
 
     return render_template('films.html', f=f)
 
